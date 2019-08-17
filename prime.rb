f81d82a1795d1cf731db11771b0db419044827a6
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-17 19:13:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
   # nums.none? { |num| num % 2 || num % 3 }
-  if num < 0
+  if num < 0 || num == 0 || num == 1
     return false
   end
   num % 2 != 0 || num % 3 != 0
