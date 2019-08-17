e03eb65a3ed0fca66590345b02480a6b699d0dc2
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-17 19:17:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  # nums.none? { |num| num % 2 || num % 3 }
+  if num < 0 || num == 0 || num == 1
+    return false
+  else 
+    (2...num).to_a.all? do |factor|
+      num % factor != 0 
+    end
+  end
+end
